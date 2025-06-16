ed435d6126692411a8298935e9d4aa7234ba4662
Johannes Feist <johannes.feist@gmail.com>
2025-06-16 14:26:02 +0200

    add optional --dt argument to plotlaserfield.f90

--- programs/plotlaserfield.f90
+++ programs/plotlaserfield.f90
@@ -4,15 +4,35 @@
 program plotlaserfield
   use laserfields
   implicit none
-  character(len=200) :: parfile
+  character(len=200) :: parfile, arg
+  ! Default value for timestep is -1, which indicates that it is not set.
+  real(dp) :: timestep = -1.d0
+  integer :: iarg
 
   parfile = 'laserfields.in'
-  if (command_argument_count() >= 1) then
-     call get_command_argument(1,parfile)
-  end if
+  iarg = 1
+  do while (iarg <= command_argument_count())
+    call get_command_argument(iarg, arg)
+    if (trim(arg) == '--dt') then
+      iarg = iarg + 1
+      call get_command_argument(iarg, arg)
+      read(arg,*) timestep
+      if (timestep <= 0) then
+        write(6,*) 'Error: Time step must be positive.'
+        stop
+      end if
+    else
+      parfile = trim(arg)
+    end if
+    iarg = iarg + 1
+  end do
   call laserfields_read_parameters(trim(parfile))
   call laserfields_write_parameters('laserfields_parameters.dat')
 
-  call write_laserfields(6)
+  if (timestep > 0) then
+    call write_laserfields(6, timestep)
+  else
+    call write_laserfields(6)
+  end if
 
 end program plotlaserfield
